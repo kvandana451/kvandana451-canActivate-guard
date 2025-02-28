ce74cb72fa80d3733d27e1d8d3bcf5fb03651241
kvandana451 <kvandana451@gmail.com>
2025-02-28 21:17:22 +0530

    rereverted

---
+++ app/src/components/role-selection/role-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-selection',
+  imports: [],
+  templateUrl: './role-selection.component.html',
+  styleUrl: './role-selection.component.scss'
+})
+export class RoleSelectionComponent {
+
+}
--- app/src/services/auth.service.ts
+++ app/src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 interface User {
   isLoggedIn: boolean;
+  role: string[];
 }
 
 @Injectable({
@@ -10,12 +11,17 @@ interface User {
 export class AuthService {
   user: User = {
     isLoggedIn: false,
+    role: ['admin', 'user'],
   };
 
   constructor(private router: Router) {}
   login() {
     this.user.isLoggedIn = true;
     localStorage.setItem('user', JSON.stringify(this.user));
+    if (this.user.role.length > 1) {
+      this.router.navigate(['/role-selection']);
+    }
+
     this.router.navigate(['/dashboard']);
     console.log(this.user);
   }
