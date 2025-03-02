83444e84f3d4ac3f88df0d6250e2728befd922da
kvandana451 <kvandana451@gmail.com>
2025-03-03 01:37:48 +0530

    enhancements final code

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,14 +1,42 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
-import { authGuard, loginGuard } from '../guards/auth.guard';
+import {
+  adminGuard,
+  authGuard,
+  loginGuard,
+  roleGuard,
+} from '../guards/auth.guard';
+import { RoleSelectionComponent } from '../components/role-selection/role-selection.component';
+import { UserDashboardComponent } from '../components/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from '../components/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [loginGuard] },
   {
-    path: 'dashboard',
-    component: DashboardComponent,
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [loginGuard],
+  },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+  //   canActivate: [authGuard],
+  // },
+  {
+    path: 'role-selection',
+    component: RoleSelectionComponent,
+    canActivate: [authGuard, roleGuard],
+  },
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboardComponent,
+    canActivate: [authGuard, adminGuard],
+  },
+
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
     canActivate: [authGuard],
   },
 ];
